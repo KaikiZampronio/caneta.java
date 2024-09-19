183a7ff0c34ba20731a32c4cc6a180218551bede
Kaiki Zampronio <160637193+KaikiZampronio@users.noreply.github.com>
2024-09-19 16:11:37 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+class Main {
+    public static void main(String[] args) {
+
+        Caneta Caneta = new Caneta("Azul", "Bic", 0.5f, 100);
+
+
+        Caneta.escrever ();
+
+
+        Caneta.destampar();
+
+
+        Caneta.escrever ();
+    }
+}
+
+
